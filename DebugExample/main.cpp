9dc2be1f1bfa11f7f5064634bc509b4af52cb105
Chuck England (FriskyDev) <crystal_bit@hotmail.com>
2019-03-12 16:55:39 -0700

    Update the debug sample.

--- DebugExample/main.cpp
+++ DebugExample/main.cpp
@@ -1,13 +1,21 @@
 #include <iostream>
 
+char crash_me(const char* const value)
+{
+    char x = value[0];
+    return x;
+}
+
 void DisplayGreeting(const char* const value)
 {
     const char* const prefix = "Hello";
     printf("%s, %s\n", prefix, value);
+
+    auto a = crash_me(nullptr);
+    printf("%c\n", a);
 }
 
 int main()
 {
     DisplayGreeting("debugger");
 }
-
