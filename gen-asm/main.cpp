f6a2172c36aa32bda384a8f08a710e1411edb85b
Chuck England (FriskyDev) <crystal_bit@hotmail.com>
2019-03-13 14:38:09 -0700

    Add updated gen-asm VS project.

--- gen-asm/main.cpp
+++ gen-asm/main.cpp
@@ -1,6 +1,40 @@
 #include <cstdio>
 #include <cstdlib>
+#include <cstdint>
 
-int main(int argc, char* argv[]) {
+void foo()
+{
+    printf("foo\n");
+}
+
+int bar()
+{
+    printf("bar\n");
+    return 42;
+}
+
+int dee(int x)
+{
+    printf("dee\n");
+    return x * 2;
+}
+
+int doo(int* x)
+{
+    printf("doo\n");
+    if (x) {
+        return *x * 4;
+    }
+    return 0;
+}
+
+int main(int argc, char* argv[], int envc, char* envv[])
+{
+    foo();
+    int x = bar();
+    x = dee(x);
+    int y = doo(&x);
+    printf("y=%d\n", y);
+
     return EXIT_SUCCESS;
 }
