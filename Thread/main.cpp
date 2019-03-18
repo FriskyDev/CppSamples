854c5b9cee93a23ec7e55a7ac50b78578865a1d9
Chuck England (FriskyDev) <crystal_bit@hotmail.com>
2019-03-18 16:13:34 -0700

    Code cleanup.

--- Thread/main.cpp
+++ Thread/main.cpp
@@ -17,15 +17,13 @@
 #endif
 
 
-
-
 template<typename ... Args>
-std::string string_format(const std::string& format, Args ... args)
+std::wstring string_format(const std::wstring& format, Args ... args)
 {
-    size_t size = snprintf(nullptr, 0, format.c_str(), args ...) + 1; // Extra space for '\0'
-    std::unique_ptr<char[]> buf(new char[size]);
-    snprintf(buf.get(), size, format.c_str(), args ...);
-    return std::string(buf.get(), buf.get() + size - 1); // We don't want the '\0' inside
+    size_t size = swprintf(nullptr, 0, format.c_str(), args ...) + 1;
+    std::unique_ptr<wchar_t[]> buf(new wchar_t[size]);
+    swprintf(buf.get(), size, format.c_str(), args ...);
+    return std::wstring(buf.get(), buf.get() + size - 1);
 }
 
 // Returns the error code as a formatted message.
@@ -59,9 +57,98 @@ void WINAPI FiberFunc(LPVOID lpParameter)
     UNREFERENCED_PARAMETER(lpParameter);
 
     // does nothing and ends
-    OutputDebugString(TEXT("Hello from the fiber\n"));
+    OutputDebugString(L"Hello from the fiber\n");
 
     SwitchToFiber(g_fiber);
+}
+
+HANDLE ghMutex;
+
+
+DWORD WINAPI WriteToDatabase(LPVOID lpParam)
+{
+    // lpParam not used in this example
+    UNREFERENCED_PARAMETER(lpParam);
+
+    DWORD dwCount = 0;
+    DWORD dwWaitResult{};
+
+    // request ownership of mutex
+    while (dwCount < 20) {
+        dwWaitResult = WaitForSingleObject(ghMutex, INFINITE);
+
+        switch (dwWaitResult) {
+            case WAIT_OBJECT_0:
+            {
+                // the thread got ownership of the mutex
+                try {
+                    // TODO: write to the database
+                    auto msg = string_format(L"Thread %d writing to database...\n", GetCurrentThreadId());
+                    OutputDebugString(msg.c_str());
+                    dwCount++;
+                }
+                catch (...) {}
+
+                // release ownership of the mutex object
+                if (!ReleaseMutex(ghMutex)) {
+                    // handle error
+                }
+            }
+            break;
+
+            case WAIT_ABANDONED:
+                // the thread got ownership of an abandoned mutex
+                // the database is in an indeterminate state
+                return FALSE;
+        }
+    }
+    return TRUE;
+}
+
+#define THREADCOUNT 20
+
+void SampleThreadsWrittingToDabase()
+{
+    HANDLE aThread[THREADCOUNT];
+    DWORD ThreadID;
+
+    // Create a mutex with no initial owner
+
+    ghMutex = CreateMutex(
+        nullptr,           // default security attributes
+        FALSE,             // initially not owned
+        nullptr);          // unnamed mutex
+    if (ghMutex == nullptr) {
+        auto msg = string_format(L"CreateMutex error: %d\n", GetLastError());
+        OutputDebugString(msg.c_str());
+        return;
+    }
+
+    // create worker threads
+    for (int i = 0; i < THREADCOUNT; i++) {
+        aThread[i] = CreateThread(
+            nullptr,    // default security attributes
+            0,          // default stack size
+            (LPTHREAD_START_ROUTINE)WriteToDatabase,
+            nullptr,    // no thread function arguments
+            0,          // default creation flags
+            &ThreadID); // receive thread identifier
+        if (aThread[i] == nullptr) {
+            auto msg = string_format(L"CreateThread error: %d\n", GetLastError());
+            OutputDebugString(msg.c_str());
+            return;
+        }
+    }
+
+    // wait for all threads to terminate
+    WaitForMultipleObjects(THREADCOUNT, aThread, TRUE, INFINITE);
+
+    // close thread and mutex handles
+    for (int i = 0; i < THREADCOUNT; i++) {
+        CloseHandle(aThread[i]);
+    }
+
+    CloseHandle(ghMutex);
 }
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
@@ -74,7 +161,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     DWORD pid = GetCurrentProcessId();
     DWORD tid = GetCurrentThreadId();
     {
-        auto msg = string_format(TEXT("Process: %d (0x%08X)  Thread: %d (0x%08X)\n"), pid, pid, tid, tid);
+        auto msg = string_format(L"Process: %d (0x%08X)  Thread: %d (0x%08X)\n", pid, pid, tid, tid);
         OutputDebugString(msg.c_str());
     }
 
@@ -84,11 +171,9 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     BOOL result = GetThreadContext(th, &ctx);
     if (!result) {
         auto err = GetLastError();
-        auto msg = GetFormatedMessage(err);
-        TCHAR buf[128];
-        wsprintf(buf, "GetThreadContext() returned 0x%08X (%d):\n", err, err);
-        msg = buf + msg;
-        MessageBox(nullptr, msg.c_str(), "Thread", MB_ICONERROR);
+        auto errMsg = GetFormatedMessage(err);
+        auto msg = string_format(L"GetThreadContext() returned 0x%08X (%d): %s\n", err, err, errMsg.c_str());
+        MessageBox(nullptr, msg.c_str(), L"Thread", MB_ICONERROR);
         return EXIT_FAILURE;
     }
 
@@ -96,10 +181,8 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     //result = Wow64GetThreadContext(th, &ctx32);
     //if (!result) {
     //    auto err = GetLastError();
-    //    auto msg = GetFormatedMessage(err);
-    //    TCHAR buf[128];
-    //    wsprintf(buf, "Wow64GetThreadContext() returned 0x%08X (%d):\n", err, err);
-    //    msg = buf + msg + "\nMust not be a 32-bit application on a 64-bit OS.";
+    //    auto errMsg = GetFormatedMessage(err);
+    //    auto msg = string_format(L"Wow64GetThreadContext() returned 0x%08X (%d): %s\n\nMust not be a 32-bit application on a 64-bit OS.", err, err, errMsg.c_str());
     //    MessageBox(nullptr, msg.c_str(), "Thread", MB_ICONERROR);
     //    return EXIT_FAILURE;
     //}
@@ -107,28 +190,28 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     g_fiber = ConvertThreadToFiber(nullptr);
     if (!g_fiber) {
         auto err = GetLastError();
-        auto msg = GetFormatedMessage(err);
-        TCHAR buf[128];
-        wsprintf(buf, "ConvertThreadToFiber() returned 0x%08X (%d):\n", err, err);
-        msg = buf + msg;
-        MessageBox(nullptr, msg.c_str(), "Thread", MB_ICONERROR);
+        auto errMsg = GetFormatedMessage(err);
+        auto msg = string_format(L"ConvertThreadToFiber() returned 0x%08X (%d): %s\n", err, err, errMsg.c_str());
+        MessageBox(nullptr, msg.c_str(), L"Thread", MB_ICONERROR);
         return EXIT_FAILURE;
     }
 
     auto child = CreateFiber(0, FiberFunc, nullptr);
     if (!child) {
         auto err = GetLastError();
-        auto msg = GetFormatedMessage(err);
-        TCHAR buf[128];
-        wsprintf(buf, "CreateFiber() returned 0x%08X (%d):\n", err, err);
-        msg = buf + msg;
-        MessageBox(nullptr, msg.c_str(), "Thread", MB_ICONERROR);
+        auto errMsg = GetFormatedMessage(err);
+        auto msg = string_format(L"CreateFiber() returned 0x%08X (%d): %s\n", err, err, errMsg.c_str());
+        MessageBox(nullptr, msg.c_str(), L"Thread", MB_ICONERROR);
         return EXIT_FAILURE;
     }
 
     SwitchToFiber(child);
 
     DeleteFiber(child);
+    OutputDebugString(L"Fiber ended and deleted\n");
+
+    OutputDebugString(L"Now showing simulated writes to database...\n");
+    SampleThreadsWrittingToDabase();
 
     return EXIT_SUCCESS;
 }
