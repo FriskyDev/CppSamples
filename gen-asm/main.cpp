6f22f3e708d4dc4310738b316eec9d5975ef6adc
Chuck England (FriskyDev) <crystal_bit@hotmail.com>
2019-03-20 22:28:49 -0700

    Update the output files to use MSBuild directories.

--- gen-asm/main.cpp
+++ gen-asm/main.cpp
@@ -1,40 +1,15 @@
-#include <cstdio>
-#include <cstdlib>
-#include <cstdint>
+#include <stdio.h>
 
-void foo()
+int main()
 {
-    printf("foo\n");
-}
-
-int bar()
-{
-    printf("bar\n");
-    return 42;
-}
-
-int dee(int x)
-{
-    printf("dee\n");
-    return x * 2;
-}
-
-int doo(int* x)
-{
-    printf("doo\n");
-    if (x) {
-        return *x * 4;
+    int n = 0, m;
+    scanf_s("%d", &m);
+    for (int i = 0; i < m; ++i) {
+        n += i;
     }
+    for (int j = 0; j < m; ++j) {
+        n += j;
+    }
+    printf("%d", n);
     return 0;
-}
-
-int main(int argc, char* argv[], int envc, char* envv[])
-{
-    foo();
-    int x = bar();
-    x = dee(x);
-    int y = doo(&x);
-    printf("y=%d\n", y);
-
-    return EXIT_SUCCESS;
 }
