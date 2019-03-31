9474ecd8e7327fca2a4f85cd56734fc53210ea45
Chuck England (FriskyDev) <crystal_bit@hotmail.com>
2019-03-31 14:13:28 -0700

    Simple program in assembly code from olc.

--- gen-asm/main.cpp
+++ gen-asm/main.cpp
@@ -1,7 +1,20 @@
 #include <stdio.h>
 
+float multiply(float a, float b)
+{
+    float c = a * b;
+    return c;
+}
+
 int main()
 {
+    float a = 10;
+    float b = 3;
+    float c = a * b;
+
+    c = multiply(a, b);
+
+
     int n = 0, m;
     scanf_s("%d", &m);
     for (int i = 0; i < m; ++i) {
